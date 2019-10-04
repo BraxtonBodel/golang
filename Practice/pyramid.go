db662577eb998bda0294d0ac4f0e4638b6c088e2
Braxton Bonilla <braxtonbonilla@gmail.com>
2019-10-04 10:25:36 -0500

    Pyramid mod

--- Practice/pyramid.go
+++ Practice/pyramid.go
@@ -4,8 +4,15 @@ import "fmt"
 
 func drawPyramid(maxRow int) {
 	for i := 1; i <= maxRow; i++ {
-		for j := 1; j <= i; j++ {
-			fmt.Print("* ")
+		if i < 7 {
+			for j := 1; j < i; j++ {
+				fmt.Print("o ")
+			}
+		} else {
+			x := (i - 6) * 2
+			for j := i; j < x; j-- {
+				fmt.Print(" o")
+			}
 		}
 		fmt.Println("")
 	}
